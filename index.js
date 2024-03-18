b1cd4321f01e88cde5b4dd6887a9bc2a6bdcb91d
lema samuel <lemasam10@gmail.com>
2024-03-18 17:25:40 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser="Lema";
+//const welcomeMessage =currentUser ;
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+
+//const excitedWelcomeMessage= "WELCOME TO FLATBOOK, ";
+//const excitedWelcomeMessage= `WELCOME TO FLATBOOK, ${currentUser}!`;
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+
+//const shortGreeting= "Welcome, "
+const shortGreeting= `Welcome, ${currentUser.slice(0,2)}!`;
